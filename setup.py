a3064f6feeabc47b01fb01e75771646400c80949
Sajjad Amiri <szamiri@percipia.com>
2016-04-26 11:08:40 -0500

    Correct urldownload and email address

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='uvp_adb',
       version='1.0',
       description='adb functions for uvp phone',
-      url='https://www.linkedin.com/in/sajjad-ziyaei-amiri-a1a16b9b',
-      author='Sajjad Ziyaei amiri',
-      author_email='szaforward@gmail.com',
+      url='https://github.com/inforfang/uvp_adb',
+      download_url = 'https://github.com/inforfang/uvp_adb/tarball/1.0',
+      author='inforfang',
+      author_email='szaforward2@gmail.com',
       license='MIT',
       packages=['uvp_adb'],
       zip_safe=False)

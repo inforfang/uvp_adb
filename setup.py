16292b68a1475bbb5d127f91167eaa108201a20a
Sajjad Amiri <szamiri@percipia.com>
2016-04-26 10:13:18 -0500

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='uvp_adb',
+      version='1.0',
+      description='adb functions for uvp phone',
+      url='https://www.linkedin.com/in/sajjad-ziyaei-amiri-a1a16b9b',
+      author='Sajjad Ziyaei amiri',
+      author_email='szaforward@gmail.com',
+      license='MIT',
+      packages=['uvp_adb'],
+      zip_safe=False)

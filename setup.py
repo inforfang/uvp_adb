383444018bc4c09ca56f3f27f9e4528d65535865
Sajjad Amiri <szamiri@percipia.com>
2016-07-06 13:42:39 -0500

    Add Tr069Service.apk as part of update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='uvp_adb',
-      version='1.3',
+      version='1.6',
       description='adb functions for uvp phone',
       url='https://github.com/inforfang/uvp_adb',
-      download_url = 'https://github.com/inforfang/uvp_adb/tarball/1.3',
+      download_url = 'https://github.com/inforfang/uvp_adb/tarball/1.6',
       author='inforfang',
       author_email='szaforward2@gmail.com',
       license='MIT',
